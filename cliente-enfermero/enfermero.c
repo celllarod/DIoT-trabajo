5f1b9c9d2469aaddad6c67b16ad5b2a248d9fe4f
celllarod <llanes.celia@icloud.com>
2024-01-11 13:25:23 +0100

    feat: enfermero sin testear

---
+++ cliente-enfermero/enfermero.c
@@ -0,0 +1,139 @@
+#include "config.h" // config especifica
+#include "contiki.h"
+#include "net/routing/routing.h"
+#include "random.h"
+#include "net/netstack.h"
+#include "net/ipv6/simple-udp.h"
+#include "lib/sensors.h"
+#include "arch/platform/nrf52840/common/temperature-sensor.h"
+#include "dev/button-hal.h"
+#include "dev/leds.h"
+
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+#define WITH_SERVER_REPLY  1
+#define UDP_CLIENT_PORT	8765
+#define UDP_SERVER_PORT	5678
+
+#define SEND_INTERVAL		  (60 * CLOCK_SECOND)
+
+#define PROCESS_EVENT_BOTON 247
+
+static struct simple_udp_connection udp_conn;
+static bool alerta = false;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(udp_client_process, "UDP client");
+PROCESS(button_hal_client_process, "Button HAL");
+
+AUTOSTART_PROCESSES(&udp_client_process, &button_hal_client_process);
+/*---------------------------------------------------------------------------*/
+static void
+udp_rx_callback(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
+
+  LOG_INFO("Recibido: '%.*s' ", datalen, (char *) data);
+  
+  if (strcmp((char *) data, "1")==0) { // 1: ALERTA
+    LOG_INFO("led_rojo");
+    leds_single_off(LEDS_LED1);
+    leds_single_on(LEDS_LED2);
+    alerta = true;
+  }
+
+  if (strcmp((char *) data, "2")==0) {//Alerta temperatura fin
+    LOG_INFO("led_verde");
+    leds_single_off(LEDS_LED2);
+    leds_single_on(LEDS_LED1);
+    
+  }
+
+
+#if LLSEC802154_CONF_ENABLED
+  LOG_INFO_(" LLSEC LV:%d", uipbuf_get_attr(UIPBUF_ATTR_LLSEC_LEVEL));
+#endif
+  LOG_INFO_("\n");
+
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(udp_client_process, ev, data)
+{
+  static struct etimer periodic_timer;
+  uip_ipaddr_t dest_ipaddr;
+//   static char str[32];
+  static bool primera_conexion = true;
+
+  PROCESS_BEGIN();
+
+ // Inicializamos el led con color verde (todo ok)
+  leds_single_on(LEDS_LED1);
+
+  /* Initialize UDP connection */
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL, UDP_SERVER_PORT, udp_rx_callback);
+
+  while(1) {
+
+      if(NETSTACK_ROUTING.node_is_reachable() && NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr)) {
+
+        if (primera_conexion == true){
+          // Enviar mensaje de que se ha conectado al servidor
+          char * msg = "CLIENTE-2";
+          LOG_INFO("Enviando msg 1ª conexion al servidor\n");
+          simple_udp_sendto(&udp_conn, msg, strlen(msg), &dest_ipaddr);
+          primera_conexion = false;
+        }
+      
+      if (alerta == true){
+        // Esperamos hasta recibir evento 
+        PROCESS_WAIT_EVENT_UNTIL((ev == PROCESS_EVENT_BOTON));
+          if (ev==PROCESS_EVENT_BOTON) {
+            LOG_INFO("Enviando respuesta de asistencia\n");
+            char *msg = "ASISTENCIA";
+            simple_udp_sendto(&udp_conn, msg, strlen(msg), &dest_ipaddr);
+          }
+
+      }
+
+
+    } else {
+      LOG_INFO("Not reachable yet\n");
+    }
+
+    /* Add some jitter */
+    etimer_set(&periodic_timer, SEND_INTERVAL- CLOCK_SECOND + (random_rand() % (2 * CLOCK_SECOND)));
+  }
+
+  PROCESS_END();
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(button_hal_client_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  while(1) {
+
+    PROCESS_YIELD();
+
+      if(ev == button_hal_press_event) {
+        LOG_INFO("Boton pulsado: asistencia en camino\n");
+        process_post(&udp_client_process, PROCESS_EVENT_BOTON, NULL);
+
+        // Fin de alerta
+        alerta = false;
+        LOG_INFO("led_verde");
+        leds_single_off(LEDS_LED2);
+        leds_single_on(LEDS_LED1);
+
+    }
+  }
+  PROCESS_END();
+}
--- servidor/servidor.c
+++ servidor/servidor.c
@@ -15,17 +15,26 @@
 #define UDP_SERVER_PORT	5678
 #define UMBRAL_TEMPERATURA 29.0
 
+#define ALERTA_CLI_2 "1"
+#define ALERTA_URGENTE_CLI_2 "2"
+
+
+#define ALERTA_TEMPERATURA_CLI_1 "1"
+#define ALERTA_TEMPERATURA_FIN_CLI_1 "2"
+
 
 static struct simple_udp_connection udp_conn;
 static uip_ipaddr_t cli1_ipaddr;
 static uip_ipaddr_t cli2_ipaddr;
 static bool alerta = false;
+static bool alerta_emergencia = false;
 
 // Declaracion de funciones
 void separarCadena(char *cadena, char *delimitador, char *partes[], int numPartes);
 
 PROCESS(udp_server_process, "UDP server");
-AUTOSTART_PROCESSES(&udp_server_process);
+PROCESS(periodic_process, "Evento periodico process");
+AUTOSTART_PROCESSES(&udp_server_process, &periodic_process);
 /*---------------------------------------------------------------------------*/
 static void
 udp_rx_callback(struct simple_udp_connection *c,
@@ -60,8 +69,7 @@ udp_rx_callback(struct simple_udp_connection *c,
 
  
   char *datos_rx[2] ={NULL, NULL};
- 
-// TODO: Problema aqui, se imprime TEMP en vez de TEMPERATURA. tiene que haber algo mal
+
   // Llamar a la función para separar la cadena
   char msg[32];
   snprintf(msg, sizeof(msg), "%s", (char *) data);
@@ -74,36 +82,43 @@ udp_rx_callback(struct simple_udp_connection *c,
   if (strcmp(datos_rx[0], "EMERGENCIA")==0){
     LOG_INFO("RX: EMERGENCIA\n"); //debug
     // Enviar al cliente 2 la alerta
-    // simple_udp_sendto(&udp_conn, "ALERTA", sizeof("ALERTA"), &cli2_ipaddr);
+    alerta_emergencia = true;
+    alerta = true;
+    simple_udp_sendto(&udp_conn, ALERTA_CLI_2, sizeof(ALERTA_CLI_2), &cli2_ipaddr); //1: ALERTA
 
   } else if (strcmp(datos_rx[0], "TEMPERATURA")==0){
     LOG_INFO("RX: TEMPERATURA\n"); //debug
     // Comprobar que no supere el umbral. Si lo supera, enviar alerta a los clientes
     if (atof(datos_rx[1])  > UMBRAL_TEMPERATURA && alerta == false) {
       // Enviar al cliente 1 la alerta para que este encienda led rojo
-      char * msg = "1"; //ALERTA_TEMPERATURA
+      char * msg = ALERTA_TEMPERATURA_CLI_1;
       simple_udp_sendto(&udp_conn, msg, strlen(msg), &cli1_ipaddr);
       alerta = true;
       LOG_INFO("Alerta temp Destino = ");
       LOG_INFO_6ADDR(sender_addr);
       LOG_INFO_("\n");
-  
+
       // Enviar al cliente 2 la alerta 
-      // simple_udp_sendto(&udp_conn, "ALERTA", sizeof("ALERTA"), &cli2_ipaddr);
+      simple_udp_sendto(&udp_conn, ALERTA_CLI_2, sizeof(ALERTA_CLI_2), &cli2_ipaddr); 
+
     }
     // Si no supera el umbral y la alerta es true, se envia al cliente para avisar de que ya no hay alerta
-    else if (atof(datos_rx[1]) < UMBRAL_TEMPERATURA && alerta == true) {
+    else if (atof(datos_rx[1]) < UMBRAL_TEMPERATURA && alerta == true && alerta_emergencia == false) {
       // Enviar al cliente 1 la alerta para que este encienda led rojo
-      char * msg = "2"; //ALERTA_TEMPERATURA_FIN
+      char * msg = ALERTA_TEMPERATURA_FIN_CLI_1; 
       simple_udp_sendto(&udp_conn, msg, strlen(msg), &cli1_ipaddr);
       alerta = false;
       LOG_INFO("Alerta fin Destino = ");
       LOG_INFO_6ADDR(sender_addr);
       LOG_INFO_("\n");
   
-      // Enviar al cliente 2 la alerta 
+      // Enviar al cliente 2 fin alerta 
       // simple_udp_sendto(&udp_conn, "ALERTA", sizeof("ALERTA"), &cli2_ipaddr);
     }
+  } else if (strcmp(datos_rx[0], "ASISTENCIA")==0) {
+    LOG_INFO("RX: ASISTENCIA\n"); //debug
+    alerta_emergencia = false;
+  
   }
 
 #endif /* WITH_SERVER_REPLY */
@@ -119,6 +134,42 @@ PROCESS_THREAD(udp_server_process, ev, data)
   /* Initialize UDP connection */
   simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL,
                       UDP_CLIENT_PORT, udp_rx_callback);
+
+  while(1) {
+
+    // Esperamos hasta recibir evento de periodic_process (fin temporizacion 10s)
+    PROCESS_WAIT_EVENT();
+    LOG_INFO("Enviando ALERTA_URGENTE a enfermero\n");
+    char * msg = ALERTA_URGENTE_CLI_2;
+    simple_udp_sendto(&udp_conn, msg, strlen(msg), &cli2_ipaddr);
+
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(periodic_process, ev, data)
+{
+  static struct etimer timer;
+
+  PROCESS_BEGIN();
+  
+  while(1) {
+    if(alerta==true){
+       LOG_INFO("Alerta true --> Temporizando 10s\n");
+      // Esperamos a que expire el timer periodico y luego lo reseteamos.
+      // Configuramos el timer periodico para que expire en 10 segundos.
+      etimer_set(&timer, CLOCK_SECOND * 10);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+      etimer_reset(&timer);
+      LOG_INFO("Fin temporizacion 10s\n");
+      if(alerta==true){
+        // Se genera evento hacia el proceso udp_server_process.
+         LOG_INFO("Alerte Sigue true --> Se envia evento fin temporizacion\n");
+        process_poll(&udp_server_process);
+      }
+    } 
+  }
 
   PROCESS_END();
 }
