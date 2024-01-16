2543d7144bb30a9c3601409248b7b38fea8751d6
celllarod <llanes.celia@icloud.com>
2024-01-16 14:20:24 +0100

    feat: servidor con fallo de temporizacion y ASISTENCIA

--- servidor/servidor.c
+++ servidor/servidor.c
@@ -11,19 +11,24 @@
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 #define WITH_SERVER_REPLY  1
-#define UDP_CLIENT_PORT	8765
+#define UDP_CLIENT_1_PORT	8765
+#define UDP_CLIENT_2_PORT	8766
 #define UDP_SERVER_PORT	5678
-#define UMBRAL_TEMPERATURA 29.0
+
+#define UMBRAL_TEMPERATURA 31.0
 
 #define ALERTA_CLI_2 "1"
 #define ALERTA_URGENTE_CLI_2 "2"
+#define ALERTA_FIN_CLI_2 "3"
 
 
 #define ALERTA_TEMPERATURA_CLI_1 "1"
 #define ALERTA_TEMPERATURA_FIN_CLI_1 "2"
 
 
-static struct simple_udp_connection udp_conn;
+#define NUM_CLIENTES 2
+
+static struct simple_udp_connection udp_conn[NUM_CLIENTES];
 static uip_ipaddr_t cli1_ipaddr;
 static uip_ipaddr_t cli2_ipaddr;
 static bool alerta = false;
@@ -33,8 +38,11 @@ static bool alerta_emergencia = false;
 void separarCadena(char *cadena, char *delimitador, char *partes[], int numPartes);
 
 PROCESS(udp_server_process, "UDP server");
+PROCESS(alerta_proccess, "Proceso que envia alerta al enfermero");
 PROCESS(periodic_process, "Evento periodico process");
-AUTOSTART_PROCESSES(&udp_server_process, &periodic_process);
+// AUTOSTART_PROCESSES(&udp_server_process, &periodic_process);
+AUTOSTART_PROCESSES(&udp_server_process, &alerta_proccess, &periodic_process);
+
 /*---------------------------------------------------------------------------*/
 static void
 udp_rx_callback(struct simple_udp_connection *c,
@@ -58,7 +66,8 @@ udp_rx_callback(struct simple_udp_connection *c,
     LOG_INFO("Direccion cliente 1 = ");
     LOG_INFO_6ADDR(&cli1_ipaddr);
     LOG_INFO_("\n");
-  } else if (strcmp((char *) data, "CLIENTE-2") == 0) {
+  } 
+  if (strcmp((char *) data, "CLIENTE-2") == 0) {
     cli2_ipaddr = *sender_addr;
     LOG_INFO("Direccion cliente 2 = ");
     LOG_INFO_6ADDR(&cli2_ipaddr);
@@ -76,49 +85,49 @@ udp_rx_callback(struct simple_udp_connection *c,
   separarCadena(msg, ":", datos_rx, 2);
 
   // Debug
-  LOG_INFO("%.*s \n", strlen(datos_rx[0]), (char *) datos_rx[0]);
-  LOG_INFO("%.*s \n", strlen(datos_rx[1]), (char *) datos_rx[1]);
+  //LOG_INFO("%.*s \n", strlen(datos_rx[0]), (char *) datos_rx[0]);
+  //LOG_INFO("%.*s \n", strlen(datos_rx[1]), (char *) datos_rx[1]);
 
   if (strcmp(datos_rx[0], "EMERGENCIA")==0){
     LOG_INFO("RX: EMERGENCIA\n"); //debug
     // Enviar al cliente 2 la alerta
     alerta_emergencia = true;
     alerta = true;
-    simple_udp_sendto(&udp_conn, ALERTA_CLI_2, sizeof(ALERTA_CLI_2), &cli2_ipaddr); //1: ALERTA
+    simple_udp_sendto(&udp_conn[1], ALERTA_CLI_2, sizeof(ALERTA_CLI_2), &cli2_ipaddr); //1: ALERTA
+    LOG_INFO("TX-CLI2: ALERTA\n"); //debug
+    LOG_INFO_6ADDR(&cli2_ipaddr);
+    LOG_INFO_("\n");
 
   } else if (strcmp(datos_rx[0], "TEMPERATURA")==0){
     LOG_INFO("RX: TEMPERATURA\n"); //debug
+
     // Comprobar que no supere el umbral. Si lo supera, enviar alerta a los clientes
     if (atof(datos_rx[1])  > UMBRAL_TEMPERATURA && alerta == false) {
       // Enviar al cliente 1 la alerta para que este encienda led rojo
       char * msg = ALERTA_TEMPERATURA_CLI_1;
-      simple_udp_sendto(&udp_conn, msg, strlen(msg), &cli1_ipaddr);
+      simple_udp_sendto(&udp_conn[0], msg, strlen(msg), &cli1_ipaddr);
+      LOG_INFO("TX-CLI1: ALERTA_TEMPERATURA\n"); //debug
       alerta = true;
-      LOG_INFO("Alerta temp Destino = ");
-      LOG_INFO_6ADDR(sender_addr);
-      LOG_INFO_("\n");
 
       // Enviar al cliente 2 la alerta 
-      simple_udp_sendto(&udp_conn, ALERTA_CLI_2, sizeof(ALERTA_CLI_2), &cli2_ipaddr); 
-
+      simple_udp_sendto(&udp_conn[1], ALERTA_CLI_2, sizeof(ALERTA_CLI_2), &cli2_ipaddr); 
+      LOG_INFO("TX-CLI2: ALERTA -- "); //debug
+      LOG_INFO_6ADDR(&cli2_ipaddr);
+      LOG_INFO_("\n");
     }
     // Si no supera el umbral y la alerta es true, se envia al cliente para avisar de que ya no hay alerta
     else if (atof(datos_rx[1]) < UMBRAL_TEMPERATURA && alerta == true && alerta_emergencia == false) {
-      // Enviar al cliente 1 la alerta para que este encienda led rojo
+      // Enviar al cliente 1 la alerta para que este encienda led verde
       char * msg = ALERTA_TEMPERATURA_FIN_CLI_1; 
-      simple_udp_sendto(&udp_conn, msg, strlen(msg), &cli1_ipaddr);
+      simple_udp_sendto(&udp_conn[0], msg, strlen(msg), &cli1_ipaddr);
       alerta = false;
-      LOG_INFO("Alerta fin Destino = ");
-      LOG_INFO_6ADDR(sender_addr);
-      LOG_INFO_("\n");
   
       // Enviar al cliente 2 fin alerta 
-      // simple_udp_sendto(&udp_conn, "ALERTA", sizeof("ALERTA"), &cli2_ipaddr);
+      simple_udp_sendto(&udp_conn[1], ALERTA_FIN_CLI_2, sizeof(ALERTA_FIN_CLI_2), &cli2_ipaddr);
     }
   } else if (strcmp(datos_rx[0], "ASISTENCIA")==0) {
     LOG_INFO("RX: ASISTENCIA\n"); //debug
     alerta_emergencia = false;
-  
   }
 
 #endif /* WITH_SERVER_REPLY */
@@ -132,19 +141,77 @@ PROCESS_THREAD(udp_server_process, ev, data)
   NETSTACK_ROUTING.root_start();
 
   /* Initialize UDP connection */
-  simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL,
-                      UDP_CLIENT_PORT, udp_rx_callback);
+  // Conexion con cliente 1
+  simple_udp_register(&udp_conn[0], UDP_SERVER_PORT, NULL,
+                      UDP_CLIENT_1_PORT, udp_rx_callback);
+  // Conexion con cliente 2
+  simple_udp_register(&udp_conn[1], UDP_SERVER_PORT, NULL,
+                      UDP_CLIENT_2_PORT, udp_rx_callback);
+  
+
+  // while(1) {
+
+  //   // Esperamos hasta recibir evento de periodic_process (fin temporizacion 10s)
+  //   PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
+  //   LOG_INFO("Enviando ALERTA_URGENTE a enfermero\n");
+  //   char * msg = ALERTA_URGENTE_CLI_2;
+  //   simple_udp_sendto(&udp_conn[1], msg, strlen(msg), &cli2_ipaddr);
+
+  // }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+// PROCESS_THREAD(alerta_proccess, ev, data)
+// {
+//   PROCESS_BEGIN();
+
+//   LOG_INFO("COMIENZA ALERTA_PROCCESS\n");  
+
+//   while(1) {
+//     LOG_INFO("ALERTA_PROCCESS\n");
+
+//     // Esperamos hasta recibir evento de periodic_process (fin temporizacion 10s)
+//     PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
+//     LOG_INFO("Enviando ALERTA_URGENTE a enfermero\n");
+//     char * msg = ALERTA_URGENTE_CLI_2;
+//     simple_udp_sendto(&udp_conn[1], msg, strlen(msg), &cli2_ipaddr);
+
+//   }
+
+//   PROCESS_END();
+// }
+PROCESS_THREAD(alerta_proccess, ev, data)
+{
+  static struct etimer periodic_timer;
+
+  PROCESS_BEGIN();
+
+  LOG_INFO("COMIENZA ALERTA_PROCCESS\n");  
+
+  etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
 
   while(1) {
+    LOG_INFO("ALERTA_PROCCESS\n");
 
-    // Esperamos hasta recibir evento de periodic_process (fin temporizacion 10s)
-    PROCESS_WAIT_EVENT();
-    LOG_INFO("Enviando ALERTA_URGENTE a enfermero\n");
-    char * msg = ALERTA_URGENTE_CLI_2;
-    simple_udp_sendto(&udp_conn, msg, strlen(msg), &cli2_ipaddr);
+    //PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer)); 
 
+    if (alerta == true) {
+      LOG_INFO("Alerta true --> Temporizar\n");
+      process_poll(&periodic_process);
+      LOG_INFO("Alerta false --> Esperando evento\n");
+      // Esperamos hasta recibir evento de periodic_process (fin temporizacion 10s)
+      PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
+      if(alerta==true){
+        LOG_INFO("Alerta sigue true --> Se envia evento fin temporizacion\n");
+        LOG_INFO("Enviando ALERTA_URGENTE a enfermero\n");
+        char * msg = ALERTA_URGENTE_CLI_2;
+        simple_udp_sendto(&udp_conn[1], msg, strlen(msg), &cli2_ipaddr);
+      }
+    } else {
+      
+    }
   }
-
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
@@ -154,23 +221,32 @@ PROCESS_THREAD(periodic_process, ev, data)
 
   PROCESS_BEGIN();
   
-  while(1) {
-    if(alerta==true){
-       LOG_INFO("Alerta true --> Temporizando 10s\n");
-      // Esperamos a que expire el timer periodico y luego lo reseteamos.
-      // Configuramos el timer periodico para que expire en 10 segundos.
-      etimer_set(&timer, CLOCK_SECOND * 10);
-      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-      etimer_reset(&timer);
-      LOG_INFO("Fin temporizacion 10s\n");
-      if(alerta==true){
-        // Se genera evento hacia el proceso udp_server_process.
-         LOG_INFO("Alerte Sigue true --> Se envia evento fin temporizacion\n");
-        process_poll(&udp_server_process);
-      }
-    } 
-  }
+  // Configuramos el timer periodico para que expire en 10 segundos.
+  etimer_set(&timer, CLOCK_SECOND * 10);
 
+  LOG_INFO("COMIENZA PROCESO TEMPORIZADOR\n");
+
+  while(1) {
+
+    PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
+    LOG_INFO("------> Temporizando 10s\n");
+    etimer_reset(&timer);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+    LOG_INFO("Fin temporizacion 10s\n");
+    // Se genera evento hacia el proceso udp_server_process.
+    process_poll(&alerta_proccess);
+    
+    
+      //  LOG_INFO("Alerta true --> Temporizando 10s\n");
+      // // Esperamos a que expire el timer periodico y luego lo reseteamos.
+      // PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+      // etimer_reset(&timer);
+      // LOG_INFO("Fin temporizacion 10s\n");
+      // if(alerta==true){
+      //   // Se genera evento hacia el proceso udp_server_process.
+      //    LOG_INFO("Alerta sigue true --> Se envia evento fin temporizacion\n");
+      //   process_poll(&alerta_proccess);
+  }
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
@@ -187,3 +263,10 @@ void separarCadena(char *cadena, char *delimitador, char *partes[], int numParte
         
     }
 }
+
+
+// TODO: Funciona todo menos la temporizacion. 
+//La idea es que uando alerta sea true se envie un evento desde alerta_proccess a periodic_process para que este temporice 10s y luego envie la alerta al enfermero.
+//EL problema es que cuando alerta es false, se queda en el proceso alerta eternamente. Tenemos que buscar una forma de que se salga.
+
+// Otro problema es que cuando enfermero envia "ASISTENCIA", el servidor no recibe el mensaje.
